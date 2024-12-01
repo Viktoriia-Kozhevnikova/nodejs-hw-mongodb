fe48368bc31473a3e72bb0ec63006ef70b4eb280
Viktoriia-Kozhevnikova <161330605+Viktoriia-Kozhevnikova@users.noreply.github.com>
2024-12-02 00:56:54 +0200

    Збереження змін на гілці hw4-validation

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -6,7 +6,7 @@ export function isValidId(req, res, next) {
     const { contactId } = req.params;
 
     if (!isValidObjectId(contactId)) {
-        return next(createHttpError.NotFound("Contact not found"));
+        return next(createHttpError(400, "Id is not valis"));
     }
 
     next();
